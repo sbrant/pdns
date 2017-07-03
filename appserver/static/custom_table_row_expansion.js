ac6149d1ee4caf02990584b631a85c59a039af74
Steve Brant <steve@brant.nu>
2017-07-03 14:23:07 -0600

    updated search and timing

--- appserver/static/custom_table_row_expansion.js
+++ appserver/static/custom_table_row_expansion.js
@@ -1,57 +1,54 @@
-require([
-    'splunkjs/mvc/tableview',
-    'splunkjs/mvc/chartview',
-    'splunkjs/mvc/searchmanager',
-    'splunkjs/mvc',
-    'underscore',
-    'splunkjs/mvc/simplexml/ready!'],function(
-    TableView,
-    ChartView,
-    SearchManager,
-    mvc,
-    _
-    ){
-
-    var EventSearchBasedRowExpansionRenderer = TableView.BaseRowExpansionRenderer.extend({
-        initialize: function(args) {
-            // initialize will run once, so we will set up a search and a chart to be reused.
-            this._searchManager = new SearchManager({
-                id: 'details-search-manager',
-                preview: false
-            });
-            this._chartView = new ChartView({
-                managerid: 'details-search-manager',
-                'charting.legend.placement': 'none'
-            });
-        },
-
-        canRender: function(rowData) {
-            // Since more than one row expansion renderer can be registered we let each decide if they can handle that
-            // data
-            // Here we will always handle it.
-            return true;
-        },
-
-        render: function($container, rowData) {
-            // rowData contains information about the row that is expanded.  We can see the cells, fields, and values
-            // We will find the sourcetype cell to use its value
-            var domCell = _(rowData.cells).find(function (cell) {
-               return cell.field === 'upper_dom';
-            //   return cell.field.toLowerCase();
-            });
-
-            //update the search with the sourcetype that we are interested in
-            this._searchManager.set({ search: '| tstats sum(DNS.bytes_in) AS totalin sum(DNS.bytes_out) AS totalout FROM datamodel=pDNS_CIM WHERE DNS.ut_domain=' + domCell.value + ' BY DNS.query'});
-
-            // $container is the jquery object where we can put out content.
-            // In this case we will render our chart and add it to the $container
-            $container.append(this._chartView.render().el);
-        }
+    require([
+        'splunkjs/mvc/tableview',
+        'splunkjs/mvc/chartview',
+        'splunkjs/mvc/searchmanager',
+        'splunkjs/mvc',
+        'underscore',
+        'splunkjs/mvc/simplexml/ready!'],function(
+        TableView,
+        ChartView,
+        SearchManager,
+        mvc,
+        _
+        ){
+        var EventSearchBasedRowExpansionRenderer = TableView.BaseRowExpansionRenderer.extend({
+            initialize: function(args) {
+                // initialize will run once, so we will set up a search and a chart to be reused.
+                this._searchManager = new SearchManager({
+                    id: 'details-search-manager',
+                    preview: true
+                });
+                this._chartView = new ChartView({
+                    managerid: 'details-search-manager',
+                    'charting.legend.placement': 'none'
+                });
+            },
+            canRender: function(rowData) {
+                // Since more than one row expansion renderer can be registered we let each decide if they can handle that
+                // data
+                // Here we will always handle it.
+                return true;
+            },
+            render: function($container, rowData) {
+                // rowData contains information about the row that is expanded.  We can see the cells, fields, and values
+                // We will find the sourcetype cell to use its value
+                //var sourcetypeCell = _(rowData.cells).find(function (cell) {
+                //   return cell.field === 'sourcetype';
+                var cellEarly = _(rowData.cells).find(function (cell) {
+                   return cell.field === 'First Query';
+                //var CellLate = _(rowData.cells).find(function (cell) {
+                //   return cell.field === 'Last Query';
+                });
+                //update the search with the sourcetype that we are interested in
+                this._searchManager.set({ search: '| datamodel pDNS_CIM DNS search | stats count by DNS.record_type', earliest_time: cellEarly, latest_time: 'now' });
+                // $container is the jquery object where we can put out content.
+                // In this case we will render our chart and add it to the $container
+                $container.append(this._chartView.render().el);
+            }
+        });
+        var tableElement = mvc.Components.getInstance("expand_with_events");
+        tableElement.getVisualization(function(tableView) {
+            // Add custom cell renderer, the table will re-render automatically.
+            tableView.addRowExpansionRenderer(new EventSearchBasedRowExpansionRenderer());
+        });
     });
-
-    var tableElement = mvc.Components.getInstance("expand_with_events");
-    tableElement.getVisualization(function(tableView) {
-        // Add custom cell renderer, the table will re-render automatically.
-        tableView.addRowExpansionRenderer(new EventSearchBasedRowExpansionRenderer());
-    });
-});
