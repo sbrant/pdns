f01966208f23a3040c631a90f374f72051a4f2a5
Steve Brant <steve@brant.nu>
2017-10-29 23:46:16 -0600

    Cleaned up code and modified search

--- appserver/static/custom_table_row_expansion.js
+++ appserver/static/custom_table_row_expansion.js
@@ -29,20 +29,31 @@
                 // Here we will always handle it.
                 return true;
             },
+    //        render: function($container, rowData) {
+    //            // Print the rowData object to the console
+    //            console.log("RowData: ", rowData);
+
+    //            // Display some of the rowData in the expanded row
+    //            $container.append('<div>'
+    //                + '<b>rowIndex</b>: ' + rowData.rowIndex + '<br>'
+    //                + '<b>colspan</b>: ' + rowData.colspan + '<br>'
+    //                + '<b>fields</b>: ' + rowData.fields + '<br>'
+    //                + '<b>values</b>: ' + rowData.values
+    //                + '</div>');
+    //        } 
             render: function($container, rowData) {
                 // rowData contains information about the row that is expanded.  We can see the cells, fields, and values
-                // We will find the sourcetype cell to use its value
-                //var sourcetypeCell = _(rowData.cells).find(function (cell) {
-                //   return cell.field === 'sourcetype';
-                var cellEarly = _(rowData.cells).find(function (cell) {
-                   return cell.field === 'First Query';
-                //var CellLate = _(rowData.cells).find(function (cell) {
-                //   return cell.field === 'Last Query';
+                // Assign the data from the desired cell to a variable for use
+                var upperDom = _(rowData.cells).find(function (cell) {
+                   return cell.field === 'upper_dom';
                 });
-                //update the search with the sourcetype that we are interested in
-                this._searchManager.set({ search: '| datamodel pDNS_CIM DNS search | stats count by DNS.record_type', earliest_time: cellEarly, latest_time: 'now' });
+                this._searchManager.set({ 
+			search: '| tstats count FROM datamodel=pDNS_CIM WHERE DNS.query=*' + upperDom.value + ' by _time span=2h',
+			earliest_time: '-5d',
+			latest_time: 'now' 
+		});
                 // $container is the jquery object where we can put out content.
-                // In this case we will render our chart and add it to the $container
+               // In this case we will render our chart and add it to the $container
                 $container.append(this._chartView.render().el);
             }
         });
